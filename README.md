f1d717773a6fcd8d88497e2e3fcc5e20fed71385
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-02-05 18:57:05 +0330

    Upload Find Prime Number project on Github

---
+++ README.md
@@ -0,0 +1,23 @@
+Program Name ===> Find The Prime Number
+
+This Program Is For Finding Prime Numbers
+
+This Program Takes A Number From The User And Checks Whether The User's Number Is The First Or Not
+
+Programming Language ===> Python
+
+Description Of Files
+
+primenum_linux.py ===> For Linux [Ubuntu Or Kali]
+
+For Run ===> python fake_linux.py
+
+primenum_windows.py ===>  For Windows
+
+For Run ===> python fake_windows.py
+
+Required packages For Python ===> os / termcolor / pyfiglet
+
+termcolor ==> To install => pip install termcolor 
+
+pyfiglet ==> To install => pip install pyfiglet
---
+++ primenum_linux.py
@@ -0,0 +1,55 @@
+# Find Prime Number
+
+# Library
+from os import system as sys
+from termcolor import colored
+from pyfiglet import figlet_format
+
+# Clear Terminal
+sys('clear')
+
+# Intro
+print(colored(figlet_format('Welcome'), color='cyan'))
+print(colored('========================================', color='red'))
+print(colored('This program for detecting prime number', color='cyan'))
+print(colored('=======================================', color='blue'))
+print('                                                             ')
+
+# While True
+while True:
+
+    # Value Input
+    number = int(input(colored('Enter Number ===> ', color='light_red')))
+    prime = True
+
+    # Loop
+    for i in range(2, number):
+        if number % i == 0:
+            prime = False
+
+    # IF For Prime Number
+    if prime:
+        print(colored(figlet_format(
+            f'{number} Is Prime'), color='green'))
+
+    # Else For Not Prime Number
+    else:
+        print(colored(figlet_format(
+            f'{number} Is Not Prime'), color='red'))
+
+    # Value For Try Again
+    con = input('Do you want try again [y/n] ===> ')
+
+    # IF For Continue
+    if con == 'y' or con == 'Y':
+        continue
+    # Elif For Break Program
+    elif con == 'n' or con == 'N':
+        break
+    # Else For Break Program
+    else:
+        break
+
+
+# Create By Moein Heshmati
+# Finish
